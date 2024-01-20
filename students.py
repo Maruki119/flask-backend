cf4df91f9c771341fdee242349bda60303546d5e
Maruki <ampamp_25@hotmail.com>
2024-01-21 02:59:06 +0700

    add more function add find specific student add update student add delete student

--- students.py
+++ students.py
@@ -22,6 +22,15 @@ def Greet():
 def get_all_students():
     all_students = collection.find()
     return jsonify({"students":[i for i in all_students]})
+
+@app.route("/students/<int:std_id>", methods = ["GET"])
+def get_student(std_id):
+    all_students = collection.find()
+    student = next( (i for i in all_students if i["_id"] == std_id), None)
+    if student:
+        return jsonify(student)
+    else:
+        return jsonify({"error": "Student not found"}), 404
     
 @app.route("/students", methods = ["POST"])
 def create_students():
@@ -37,6 +46,27 @@ def create_students():
         return jsonify(new_student), 200
     except Exception as e:
         return jsonify({"error":"Cannot create new student"}), 500
+    
+@app.route("/students/<int:std_id>", methods = ["PUT"])
+def update_student(std_id):
+    all_students = collection.find()
+    student = next( (i for i in all_students if i["_id"] == std_id), None)
+    if student:
+        data = request.get_json()
+        collection.update_one({"_id": std_id}, {"$set": data})
+        return jsonify(student), 200
+    else:
+        return jsonify({"error": "Student not found"}), 404
+
+@app.route("/students/<int:std_id>", methods = ["DELETE"])
+def delete_student(std_id):
+    all_students = collection.find()
+    student = next( (i for i in all_students if i["_id"] == std_id), None)
+    if student:
+        collection.delete_one({"_id": std_id})
+        return jsonify({"message": "Student deleted successfully"}), 200
+    else:
+        return jsonify({"error": "Student not found"}), 404
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5000,debug=True)
+    app.run(host = "0.0.0.0", port = 5000, debug = True)
